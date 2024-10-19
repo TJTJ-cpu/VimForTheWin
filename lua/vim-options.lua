32031dd7ec960be45e82e7d5d1ac238aa32e80ad
TJTJ-cpu <117895770+TJTJ-cpu@users.noreply.github.com>
2024-10-19 18:06:27 +0200

    Added the proper file

---
+++ lua/vim-options.lua
@@ -0,0 +1,48 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set rnu")
+vim.cmd("set nu")
+
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.g.mapleader = " "
+
+-- noremap: Reursive mapping
+--  silent: Display Message
+
+-- TJ's 
+vim.api.nvim_set_keymap("n", "<", "la", {})
+
+-- Quick
+vim.api.nvim_set_keymap("n", "H", "0", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "H", "0", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>v", ":vsplit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>pv", ":Ex<CR>", { noremap = true, silent = true })
+
+-- Save
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+
+-- Quit
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>qa", ":qa<CR>", { noremap = true, silent = true })
+
+-- Spilt Screen
+vim.api.nvim_set_keymap("n", "<leader>v", ":vsplit<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><Tab>", "<C-w>w", { noremap = true, silent = true })
+
+-- Yank to System Clipboard
+vim.api.nvim_set_keymap("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>Y", '"+Y', { noremap = true, silent = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- Paste from System Clipboard
+vim.api.nvim_set_keymap("n", "<leader>zp", '"+p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>zp", '"+p', { noremap = true, silent = true })
